6a05bd167b1b1ebf94c50fd8a558127ba1675d7f
soumya816-us <soumyareddy816@gmail.com>
2025-01-01 19:15:06 -0500

    modified 03-variables-replace file

---
+++ 03-variables-replace.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=ramu
+PERSON2=rahul
+
+echo "${PERSON1}:: hi ${PERSON2}"
+echo "${PERSON2}:: hi ${PERSON1}"
+echo "${PERSON1}:: how are you ${PERSON2}"
+echo "${PERSON2}:: am good,how are you ${PERSON1}"
