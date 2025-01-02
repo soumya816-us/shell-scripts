88e71f14b176283a9c27cc9b82a42afe3521d862
soumya816-us <soumyareddy816@gmail.com>
2025-01-01 19:26:02 -0500

    passing args 04-variables-args added

---
+++ 04-variables-args.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#pass args while executing this shell script
+
+PERSON1=$1
+PERSON2=$2
+
+echo "${PERSON1}:: hi ${PERSON2}"
+echo "${PERSON2}:: hi ${PERSON1}"
+echo "${PERSON1}:: how are you ${PERSON2}"
+echo "${PERSON2}:: am good,how are you ${PERSON1}"
