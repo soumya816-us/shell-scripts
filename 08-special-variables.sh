6b1cf96b2c1412411e48d931928daaf0fd7c9024
soumya816-us <soumyareddy816@gmail.com>
2025-01-03 00:31:29 -0500

    special variables added

---
+++ 08-special-variables.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#--------special variables-----
+echo " All the Variables :: $@ "
+
+echo " Number of Variables:: $# "
+
+echo "script Name:: $0 "
+
+echo " Present working directory:: $PWD "
+
+echo " Home directory current User :: $HOME "
+
+echo " which user is running the script :: $USER "
+
+echo " process id of current script :: $$ "
+
+sleep 60 &
+
+echo "process id of last command in backgroung :: $! "
