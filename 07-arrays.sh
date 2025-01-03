ef52346b59fb2d2ef8ff2d2e5e6bdda606805220
soumya816-us <soumyareddy816@gmail.com>
2025-01-02 19:51:58 -0500

    arrays added

---
+++ 07-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES={"PUSHPA" "RRR" "DEVARA"}
+
+echo "First Movie :: ${MOVIES[0]}"
+echo "Second Movie :: ${MOVIES[1]}"
+echo "Third Movie :: ${MOVIES[2]}"
+
+echo "All Movies are:: ${MOVIES[@]}"
