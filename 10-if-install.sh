a52ce4c94cd0ad8647529b38557a4dea893a7f2e
soumya816-us <soumyareddy816@gmail.com>
2025-01-03 19:41:31 -0500

    installing packages using shell

--- 10-if-install.sh
+++ 10-if-install.sh
@@ -5,6 +5,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
 echo "ERROR:: you must have sudo access to execute script"
+exit 1 # other than exit 0 you can give any number "exit 0 means success"
 fi
 
 
