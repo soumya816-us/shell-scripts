62b83261cc57c0b44fbd3aad6739d15cb28a5940
soumya816-us <soumyareddy816@gmail.com>
2025-01-03 20:09:49 -0500

    installing packages using shell

--- 10-if-install.sh
+++ 10-if-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-USERID=$(id -u)
+USERID=$(id -u) # checking is it root user or not
 
 if [ $USERID -ne 0 ]
 then 
@@ -8,23 +8,44 @@ then
     exit 1 # other than exit 0 you can give any number "exit 0 means success"
 fi
 
-
-dnf install mysql -y
-
-if [ $? -ne 0 ]
-then 
-    echo " installing mysql .... Failure "
-    exit 1
-else
-    echo " installing mysql .... Success "
-fi
-
-dnf install git -y
+dnf list installed git # before installing mysql checking it is installed before or not
 
 if [ $? -ne 0 ]
-then 
-    echo " installing Git .... Failure "
-    exit 1
+then
+    dnf install mysql -y
+
+    if [ $? -ne 0 ] # again checking installation of mysql is success or not
+    then 
+        echo " installing mysql .... Failure "
+        exit 1
+    else
+        echo " installing mysql .... Success "
+    fi
 else
-    echo " installing Git .... Success "
+ echo " Mysql is Already Installed"
+ fi
+
+
+# if [ $? -ne 0 ]
+# then 
+#     echo " installing mysql .... Failure "
+#     exit 1
+# else
+#     echo " installing mysql .... Success "
+# fi
+
+dnf list installed git # before installing git checking it is installed before or not
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    if [ $? -ne 0 ] #again checking installation of mysql is success or not
+    then 
+        echo " installing Git .... Failure "
+        exit 1
+    else
+        echo " installing Git .... Success "
+    fi
+else
+echo "Git is Already Installed"
 fi
