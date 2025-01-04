4792c7d44e2333c60f57eb33781cb57ff87e4a1e
soumya816-us <soumyareddy816@gmail.com>
2025-01-03 20:38:48 -0500

    functions using shell

---
+++ 11-functions.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+USERID=$(id -u) # checking is it root user or not
+
+
+VALIDATE(){
+
+ if [ $1 -ne 0 ] # again checking installation is success or not
+    then 
+        echo " $2 .... Failure "
+        exit 1
+    else
+        echo " $2 .... Success "
+    fi
+
+}
+
+
+#USERID=$(id -u) # checking is it root user or not
+
+if [ $USERID -ne 0 ] 
+then 
+    echo "ERROR:: you must have sudo access to execute script"
+    exit 1 # other than exit 0 you can give any number "exit 0 means success"
+fi
+
+dnf list installed git # before installing mysql checking it is installed before or not
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    VALIDATE $? "INSTALLING MYSQL"
+   
+else
+ echo " Mysql is Already Installed"
+ fi
+
+
+dnf list installed git # before installing git checking it is installed before or not
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    VALIDATE $? "INSTALLING Git"
+else
+echo "Git is Already Installed"
+fi
