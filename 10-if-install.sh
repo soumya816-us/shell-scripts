f92d2e89c75889b3d8f88adf554acf32483eea9a
soumya816-us <soumyareddy816@gmail.com>
2025-01-03 19:52:36 -0500

    installing packages using shell

--- 10-if-install.sh
+++ 10-if-install.sh
@@ -4,11 +4,27 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then 
-echo "ERROR:: you must have sudo access to execute script"
-exit 1 # other than exit 0 you can give any number "exit 0 means success"
+    echo "ERROR:: you must have sudo access to execute script"
+    exit 1 # other than exit 0 you can give any number "exit 0 means success"
 fi
 
 
 dnf install mysql -y
 
+if [ $? -ne 0 ]
+then 
+    echo " installing mysql .... Failure "
+    exit 1
+else
+    echo " installing mysql .... Success "
+fi
+
 dnf install git -y
+
+if [ $? -ne 0 ]
+then 
+    echo " installing Git .... Failure "
+    exit 1
+else
+    echo " installing Git .... Success "
+fi
