ef193692ad8028cde37b613bf1ea8474fa3cea14
soumya816-us <soumyareddy816@gmail.com>
2025-01-05 01:07:53 -0500

    delete logs shell

--- 16-delete-logs.sh
+++ 16-delete-logs.sh
@@ -37,5 +37,8 @@ fi
 
 echo "script started executing at :: $TIMESTAMP" &>>$LOG_FILE_NAME
 
+mkdir -p /var/log/shellscript-logs
+VALIDATE $? "Script logs Created"
+
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 echo  "Files to be deleted: $FILES_TO_DELETE"
