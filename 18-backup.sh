47f791755e2facf86e59f5b0dfd8d533d3c1a69a
soumya816-us <soumyareddy816@gmail.com>
2025-01-06 17:39:02 -0500

    backup shellscript added

---
+++ 18-backup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#colors added to the script
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} # if user not providing number of days,we are taking 14days
+
+
+
+LOGS_FOLDER="/home/ec2-user/var/log/app-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+
+VALIDATE(){
+
+ if [ $1 -ne 0 ] # again checking installation is success or not
+    then 
+        echo -e " $2 .... $R Failure $N "
+        exit 1
+    else
+        echo -e " $2 .... $G Success $N "
+    fi
+
+}
+mkdir -p /var/log/app-logs
+
+USAGE()
+{
+    echo -e " $R USAGE :: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(OPTIONAL)>"
+}
+
+
+if[ $# -lt 2 ]
+then
+    USAGE
+fi
+
+echo "script started executing at :: $TIMESTAMP" &>>$LOG_FILE_NAME
