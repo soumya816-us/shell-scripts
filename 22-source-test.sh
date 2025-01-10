348c0027db674cbfb0478343ed4bda99264ebdb0
soumya816-us <soumyareddy816@gmail.com>
2025-01-10 00:36:01 -0500

    shellscript added

---
+++ 22-source-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source ./common.sh
+
+echo "script started executing at :: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+echo  "Files to be deleted: $FILES_TO_DELETE"
+
+while read -r file 
+do
+echo "Deleting file: $file "
+rm -rf $file
+done <<< $FILES_TO_DELETE
---
+++ common.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USERID=$(id -u) # checking is it root user or not
+
+#colors added to the script
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR="/home/ec2-user/app-logs"
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+
+VALIDATE(){
+
+ if [ $1 -ne 0 ] # again checking installation is success or not
+    then 
+        echo -e " $2 .... $R Failure $N "
+        exit 1
+    else
+        echo -e " $2 .... $G Success $N "
+    fi
+
+}
+
+CHECK_ROOT(){
+if [ $USERID -ne 0 ] 
+then 
+    echo "ERROR:: you must have sudo access to execute script"
+    exit 1 # other than exit 0 you can give any number "exit 0 means success"
+fi
+}
