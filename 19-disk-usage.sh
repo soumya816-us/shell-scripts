f51767477d453a8e07da80f845a61f2914a47cf9
soumya816-us <soumyareddy816@gmail.com>
2025-01-09 19:09:56 -0500

    disk usage shellscript added

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -2,10 +2,15 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOULD=5 #in real projects will monitor for 70
-
+MSG=""
 while read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk -F " " '{print $NF}')
     echo "Partition: $PARTITION , Usage: $USAGE "
+    if [ $USAGE -ge $DISK_THRESHOULD]
+    then
+        MSG="High Disk Usage on partition: $PARTITION Usage is $Usage"
 done <<< $DISK_USAGE
+
+echo "Message: $MSG"
