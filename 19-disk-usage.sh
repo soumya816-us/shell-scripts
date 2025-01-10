c30ea925bfce08b21e00b4337ba43a2b0dde4b6e
soumya816-us <soumyareddy816@gmail.com>
2025-01-09 19:05:05 -0500

    disk usage shellscript added

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -6,4 +6,6 @@ DISK_THRESHOULD=5 #in real projects will monitor for 70
 while read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}')
+    PARTITION=$(echo $line | awk -F " " '{print $NF}')
+    echo "Partition: $PARTITION , Usage: $USAGE "
 done <<< $DISK_USAGE
