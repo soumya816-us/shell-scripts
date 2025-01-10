7686269594ae5f81daa58e9ec05f98dd2079ae4a
soumya816-us <soumyareddy816@gmail.com>
2025-01-09 19:02:29 -0500

    disk usage shellscript added

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOULD=5 #in real projects will monitor for 70
 
 while read -r line
 do
-    echo $line
+    USAGE=$(echo $line | awk -F " " '{print $6F}')
 done <<< $DISK_USAGE
